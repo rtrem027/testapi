d9bc5299617a030a67e4347a1d57a63070c118a3
rtrem027 <renato.roriz@outlook.com>
2022-08-23 20:34:10 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/controller/PessoaController.java
@@ -0,0 +1,69 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Pessoa;
+import com.example.demo.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+public class PessoaController {
+        @Autowired
+        private PessoaRepository _pessoaRepository;
+    private RequestMethod Request;
+
+    @RequestMapping(value = "/pessoa", method = RequestMethod.GET)
+        public List<Pessoa> Get() {
+            return _pessoaRepository.findAll();
+        }
+
+        @RequestMapping(value = "/pessoa/{id}", method = RequestMethod.GET)
+        public ResponseEntity<Pessoa> GetById(@PathVariable(value = "id") long id)
+        {
+            Optional<Pessoa> pessoa = _pessoaRepository.findById(id);
+            if(pessoa.isPresent())
+                return new ResponseEntity<Pessoa>(pessoa.get(), HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        @RequestMapping(value = "/pessoa", method =  RequestMethod.POST)
+        public Pessoa Post(@Valid @RequestBody Pessoa pessoa)
+        {
+            return _pessoaRepository.save(pessoa);
+        }
+
+        @RequestMapping(value = "/pessoa/{id}", method =  RequestMethod.PUT)
+        public ResponseEntity<Pessoa> Put(@PathVariable(value = "id") long id, @Valid @RequestBody Pessoa newPessoa)
+        {
+            Optional<Pessoa> oldPessoa = _pessoaRepository.findById(id);
+            if(oldPessoa.isPresent()){
+                Pessoa pessoa = oldPessoa.get();
+                pessoa.setNome(newPessoa.getNome());
+                _pessoaRepository.save(pessoa);
+                return new ResponseEntity<Pessoa>(pessoa, HttpStatus.OK);
+            }
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        @RequestMapping(value = "/pessoa/{id}", method = RequestMethod.DELETE)
+        public ResponseEntity<Object> Delete(@PathVariable(value = "id") long id)
+        {
+            Optional<Pessoa> pessoa = _pessoaRepository.findById(id);
+            if(pessoa.isPresent()){
+                _pessoaRepository.delete(pessoa.get());
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
---
+++ src/main/java/com/example/demo/entity/Pessoa.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
+
